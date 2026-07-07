9da0d4cf9c48f30ba0e1f33f39e8c359f77ea9e3
JoseMa-gif <0333222005@unjfsc.edu.pe>
2026-07-07 13:08:35 -0500

    Se agrega la clase de conexión

---
+++ Sistem_Matricula/src/main/java/com/matricula/util/ConexionBD.java
@@ -0,0 +1,34 @@
+package com.matricula.util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConexionBD {
+
+    private static HikariDataSource dataSource;
+
+    // TODO: mover estos valores a un archivo de configuración externo (no subir credenciales al repositorio)
+    private static final String HOST = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "localhost";
+    private static final String URL = "jdbc:mysql://" + HOST + ":3306/bd_matricula?useSSL=false&serverTimezone=America/Lima";
+    private static final String USUARIO = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
+    private static final String CLAVE = System.getenv("DB_PASS") != null ? System.getenv("DB_PASS") : "";
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(URL);
+        config.setUsername(USUARIO);
+        config.setPassword(CLAVE);
+        config.setMaximumPoolSize(10);
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource = new HikariDataSource(config);
+    }
+
+    private ConexionBD() {
+    }
+
+    public static Connection getConexion() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
