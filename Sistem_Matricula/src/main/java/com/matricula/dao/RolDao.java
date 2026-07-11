6682430bb6875938dcf8b09b294596b10b8ddb38
JoseMa-gif <0333222005@unjfsc.edu.pe>
2026-07-11 17:59:56 -0500

    se creo RolDAO.java

---
+++ Sistem_Matricula/src/main/java/com/matricula/dao/RolDao.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.matricula.dao;
+
+import com.matricula.model.Rol;
+import com.matricula.util.ConexionBD;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author JOSE
+ */
+public class RolDao {
+    
+    public List<Rol> listarTodos() throws SQLException {
+        List<Rol> lista = new ArrayList<>();
+        String sql = "SELECT * FROM rol ORDER BY idRol";
+        try (Connection con = ConexionBD.getConexion();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Rol r = new Rol();
+                r.setIdRol(rs.getInt("idRol"));
+                r.setNombreRol(rs.getString("nombreRol"));
+                r.setEstado(rs.getBoolean("estado"));
+                lista.add(r);
+            }
+        }
+        return lista;
+    }
+    
+}
